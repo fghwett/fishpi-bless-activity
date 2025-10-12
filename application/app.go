8e82f9fb31864be7f190870c770010f1214cf267
fghwett <1058178245@qq.com>
2025-10-12 13:00:59 +0800

    增加内容网页

--- application/app.go
+++ application/app.go
@@ -4,8 +4,11 @@ import (
 	"bless-activity/service"
 	"bless-activity/service/fishpi"
 	"log/slog"
+	"net/http"
+	"os"
 
 	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/apis"
 	"github.com/pocketbase/pocketbase/core"
 )
 
@@ -15,12 +18,25 @@ import (
 	后端：定时统计文章信息 为作者创建账号 更新文章排行
     作者登陆账号 进行赠送祝福和博饼
 
+    /login 登陆
+    /callback 回调
+
+    /me name nickname avatar base_times extra_times rest_times article_id
+    /rank 排行榜
+    /awards 博饼奖励内容 剩余数量
+    /play 博饼参与
+    /histories 博饼记录
+    /vote 投票
+    /vote/list 投票记录
+
+
 
 */
 
 type Application struct {
 	app *pocketbase.PocketBase
 
+	fishPiService  *fishpi.Service
 	articleService *service.ArticleService
 }
 
@@ -52,14 +68,14 @@ func (application *Application) Start() error {
 func (application *Application) init(event *core.BootstrapEvent) error {
 	event.App.Logger().Debug("初始化程序")
 
-	fishPiService, err := fishpi.NewService(event.App)
-	if err != nil {
+	var err error
+	if application.fishPiService, err = fishpi.NewService(event.App); err != nil {
 		event.App.Logger().Error("创建fishPi Service失败", slog.Any("err", err))
 		return err
 	}
 
 	// 文章爬取服务
-	application.articleService = service.NewArticleService(event.App, fishPiService)
+	application.articleService = service.NewArticleService(event.App, application.fishPiService)
 	go application.articleService.FetchArticles()
 
 	// 注册路由
@@ -71,6 +87,15 @@ func (application *Application) init(event *core.BootstrapEvent) error {
 
 func (application *Application) registerRoutes() {
 	application.app.OnServe().BindFunc(func(event *core.ServeEvent) error {
+
+		event.Router.GET("/test", func(e *core.RequestEvent) error {
+			return e.String(http.StatusOK, "test")
+		})
+		event.Router.GET("/{path...}", apis.Static(os.DirFS("./pb_public"), false))
+
+		event.Router.GET("/test1", func(e *core.RequestEvent) error {
+			return e.String(http.StatusOK, "test1")
+		})
 
 		return event.Next()
 	})
